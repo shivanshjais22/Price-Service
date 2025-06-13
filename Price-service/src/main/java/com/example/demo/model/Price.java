845939c47109b70aa6459d97d02e9439e1ad8b21
shivanshjais22 <jaisshivansh85@gmail.com>
2025-06-13 16:39:42 +0530

    Add Attribute

--- Price-service/src/main/java/com/example/demo/model/Price.java
+++ Price-service/src/main/java/com/example/demo/model/Price.java
@@ -1,9 +1,34 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name = "yoyo")
 public class Price {
-
-	private void syso() {
-		// TODO Auto-generated method stub
-
+    @Id
+	 private Long productId;
+	    public Long getProductId() {
+		return productId;
 	}
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+	public double getBasePrice() {
+		return basePrice;
+	}
+	public void setBasePrice(double basePrice) {
+		this.basePrice = basePrice;
+	}
+	public double getDiscountPercent() {
+		return discountPercent;
+	}
+	public void setDiscountPercent(double discountPercent) {
+		this.discountPercent = discountPercent;
+	}
+		private double basePrice;
+	    private double discountPercent;
 }
